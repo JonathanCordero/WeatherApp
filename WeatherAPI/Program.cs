043001374c30586a577d7d5838c9f85ae2725b8e
JonathanCordero <Corderoj25@gmail.com>
2024-11-25 11:11:46 -0500

    Starting on the logic for backend now that I know It can properly talk to the browser.

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -1,41 +1,42 @@
+//need to tackle warning.
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather API", Version = "v1" , Description = "Documentation for weather App Api."});
+});//provides swagger documentation for API. Research more on the definition later.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage(); // Show detailed errors in development
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>{
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API v1");
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
+app.MapGet("/weatherforecast", () =>{
+    var summaries = new[]{
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>new WeatherForecast(
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+            summaries[Random.Shared.Next(summaries.Length)])).ToArray();
     return forecast;
-})
-.WithName("GetWeatherForecast");
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary){
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
